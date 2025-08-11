d710ff147de4edae87ee4f059e5c9b2ea522ebbc
Nic Masters <nic.masters@gmail.com>
2025-08-11 22:38:38 +0100

    Update .vimrc Update setup.sh

--- run_once_setup.sh
+++ run_once_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-chsh -s $(which zsh)
 
 mkdir -p ~/.local/state/vim/{backup,swap,undo}
+
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+chsh -s /bin/zsh
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
