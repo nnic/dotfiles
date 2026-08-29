214efd574b6a76612851dc635a2e05aaddb7075f
Nic Masters <nic.masters@gmail.com>
2026-08-29 23:53:24 +0100

    Add .config/lazyvim/lua/config/blink.lua

---
+++ dot_config/lazyvim/lua/config/blink.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = {
+          border = "rounded",
+          winhighlight = "Normal:BlinkCmpMenu,FloatBorder:BlinkCmpMenuBorder,CursorLine:BlinkCmpMenuSelection,Search:None",
+        },
+        documentation = {
+          window = {
+            border = "rounded",
+            winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
+          },
+        },
+      },
+    },
+  },
+}
